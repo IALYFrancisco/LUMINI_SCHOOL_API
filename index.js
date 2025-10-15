394a26a0faa0bbe857b5fcc7384592e06b21d3e5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-15 04:37:08 +0300

    ajout de midlleware qui parse les corps de requête en json

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { app_routes } from "./app/routes/index.js"
 
 const app = e()
 
+app.use(e.json())
 app.use('/', app_routes)
 
 app.listen(3000, ()=>{ console.log("Server is runnning at 3000") })
