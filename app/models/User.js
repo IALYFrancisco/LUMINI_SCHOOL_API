4e71db6cc85244b9d3e07e480f05d7a0de6ca950
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-15 09:09:41 +0300

    ajout d'attribut date à la classe User

--- app/models/User.js
+++ app/models/User.js
@@ -4,7 +4,8 @@ const UserSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     status: { type: String, required: true, default: "user" },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    registerDate: { type: Date, required: true, default: Date.now() }
 })
 
 export const User = new model('Users', UserSchema)
