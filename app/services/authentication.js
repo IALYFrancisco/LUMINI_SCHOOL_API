a61d59d0e20c3a9abe1cd48b7656b41e5418193e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-15 09:06:18 +0300

    amélioraton du service d'inscription

--- app/services/authentication.js
+++ app/services/authentication.js
@@ -3,10 +3,15 @@ import { User } from "../models/User.js";
 export async function Register(request, response) {
     try {
         if(request.body){
-            let newUser = User(request.body)
-            await newUser.save()
-            if(newUser){
-                response.status(201).end()
+            let user = await User.findOne({ email: request.body.email })
+            if(user){
+                response.status(409).end()
+            }else{
+                let newUser = User(request.body)
+                await newUser.save()
+                if(newUser){
+                    response.status(201).end()
+                }
             }
         }else{
             response.status(401).json('Request body isn\'t defined.')
