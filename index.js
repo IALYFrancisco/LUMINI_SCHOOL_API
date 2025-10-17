7affb0680c9d37b539427740141ffe0c38bf4351
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-17 07:57:53 +0300

    configuration de session des connexions des utilisateurs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { app_routes } from "./app/routes/index.js"
 import { config } from "dotenv"
 import { DbConnection } from "./app/services/db_connection.js"
 import session from "express-session"
+import MongoStore from "connect-mongo"
 
 const app = e()
 
@@ -13,7 +14,13 @@ app.use(e.json())
 app.use('/', app_routes)
 
 app.use(session({
-    secret: process.env.SESSION_SECRET
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.DB_URI,
+        ttl: 14 * 24 * 60 * 60
+    })
 }))
 
 app.listen(3000, () =>{ console.log("Server is runnning at 3000") })
