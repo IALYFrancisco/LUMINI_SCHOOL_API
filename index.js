734b0a25bf805d32e07229e3141a4088e228e9af
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-17 07:08:57 +0300

    configuration de session de connexion des utilisateurs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import e from "express"
 import { app_routes } from "./app/routes/index.js"
 import { config } from "dotenv"
 import { DbConnection } from "./app/services/db_connection.js"
+import session from "express-session"
 
 const app = e()
 
@@ -10,5 +11,9 @@ DbConnection()
 
 app.use(e.json())
 app.use('/', app_routes)
+
+app.use(session({
+    secret: process.env.SESSION_SECRET
+}))
 
 app.listen(3000, () =>{ console.log("Server is runnning at 3000") })
