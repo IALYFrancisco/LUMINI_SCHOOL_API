13afee6404d508b244c46e2bdd0c1f4d535b1002
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-22 12:53:37 +0300

    création du modèle de donnée pour les formations

---
+++ app/models/Formation.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+const formationSchema = new mongoose.Schema({
+    image: { type: String, required: true },
+    title: { type: String, required: true },
+    publisheDate: { type: Date, default: null },
+    description: { type: String, required: true },
+    prerequis: { type: [String], default: [] },
+    published: { type: Boolean, required: true, default: false }
+},{timestamps: true})
+
+const Formation = new mongoose.model('Formations', formationSchema)
