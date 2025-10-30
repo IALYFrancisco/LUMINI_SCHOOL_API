cc326c13b7575bf4278bf6a817e77b8697102f6a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-30 20:14:15 +0300

    configuration de l'upload de fichier

--- app/services/formation.js
+++ app/services/formation.js
@@ -1,4 +1,23 @@
+import multer, { memoryStorage } from "multer"
+
 export async function AddFormation(request, response) {
     console.log(request.body)
     response.json(request.body)
 }
+
+const storage = memoryStorage()
+
+const fileFilter = (request, file, cb)=>{
+    const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg']
+    if(allowedTypes.includes(file.mimetype)){
+        cb(null, true)
+    }else{
+        cb(new Error("File type not allowed."), false)
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: { fileSize: 15*1024*1024}
+})
