01a969ad739cdeffd7a3262f3cecfe7d395bbf4a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-30 21:14:49 +0300

    fonctionnalité d'ajout de formation mise en place

--- app/services/formation.js
+++ app/services/formation.js
@@ -2,10 +2,25 @@ import e from "express"
 import multer, { memoryStorage } from "multer"
 import path from "path"
 import { fileURLToPath } from 'url'
+import { Formation } from "../models/Formation.js"
+import sharp from "sharp"
 
 export async function AddFormation(request, response) {
-    console.log(request.file)
-    response.json(request.file)
+    try{
+        if(!request.file) return response.status(400).end()
+            let fileName = `${Date.now()}-${Math.round(Math.random()*1E9)}.jpeg`
+            let newFormation = new Formation(request.body)
+            newFormation.image = `formations/${fileName}`
+            let result = await newFormation.save()
+            if(result){
+                let output = `./app/public/formations/${fileName}`
+                await sharp(request.file.buffer).jpeg({ quality: 60 }).toFile(output)
+            }
+            response.status(201).json(result)
+    }catch(err){
+        response.status(500).json(err)
+    }
+    response.json(request.body)
 }
 
 const storage = memoryStorage()
