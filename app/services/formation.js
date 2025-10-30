01c96d342adbb32eeb49e256ecffca4dfdc79f8f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-10-30 17:59:17 +0300

    développement de la fonctionnalité d'ajout de formation

--- app/services/formation.js
+++ app/services/formation.js
@@ -1,3 +1,4 @@
 export async function AddFormation(request, response) {
-    response.status(200).json({message: "Do you want to add a formation?"})
+    console.log(request.body)
+    response.json(request.body)
 }
