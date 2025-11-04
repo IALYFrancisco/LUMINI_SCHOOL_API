8f35e52a0d917b4419a326334f6b4c843e72b140
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-04 13:09:14 +0300

    ajout d'autorisation CORS avec les crédentials

--- app/services/cors.js
+++ app/services/cors.js
@@ -1,5 +1,6 @@
 export const corsConfiguration = (request, response, next) => {
     response.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN)
     response.setHeader('Access-Control-Allow-Headers', process.env.ALLOWED_HEADERS)
+    response.setHeader('Access-Control-Allow-Credentials', true)
     next()
 }
