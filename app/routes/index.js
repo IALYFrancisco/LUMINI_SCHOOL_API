d204555d05c5d155f9a8c72309d3c51eb607ba7e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-06 21:20:59 +0300

    construction de l'endpoint de récupération des informations d'utilisateur

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,10 @@
 import e from "express"
 import { authentication_router } from "./authentication.js"
 import { formationRouter } from "./formation.js"
+import { GetUserInformations } from "../services/user.js"
 
 export const app_routes = e.Router()
 
 app_routes.use('/authentication', authentication_router)
 app_routes.use('/formation', formationRouter)
+app_routes.use('/user', GetUserInformations)
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,5 +1,7 @@
 import Router from 'express'
+import { isAuthenticated } from '../services/authentication'
+import { GetUserInformations } from '../services/user'
 
 export const userRouter = Router()
 
-userRouter.get('/informations')
+userRouter.get('/informations', isAuthenticated, GetUserInformations)
