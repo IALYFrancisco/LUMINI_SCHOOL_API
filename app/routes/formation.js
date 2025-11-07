8ae32e37c53a3f5ca6072787d7679f7217c69d01
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-07 13:03:51 +0300

    ajout de middleware 'isAuthenticated' aux endpoints d'ajout et de suppression de formation

--- app/routes/formation.js
+++ app/routes/formation.js
@@ -1,8 +1,9 @@
 import Router from 'express'
+import { isAuthenticated } from '../services/authentication.js'
 import { AddFormation, DeleteFormation, GetFormation, upload } from '../services/formation.js'
 
 export const formationRouter = Router()
 
-formationRouter.post('/add', upload.single("poster"), AddFormation)
+formationRouter.post('/add', isAuthenticated, upload.single("poster"), AddFormation)
 formationRouter.get('/get', GetFormation)
-formationRouter.delete('/delete', DeleteFormation)
+formationRouter.delete('/delete', isAuthenticated, DeleteFormation)
