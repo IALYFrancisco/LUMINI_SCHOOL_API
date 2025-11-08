c2496ae3a7a227bd40f27dadc7b75c5d2990fba5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-08 15:20:48 +0300

    service de création d'inscription défini

---
+++ app/services/registration.js
@@ -0,0 +1,15 @@
+import { Registration } from "../models/Registration"
+
+export async function CreateRegistration(request, response) {
+    try{
+       let registration = new Registration(request.body)
+       let result = await registration.save()
+       if(result){
+            response.status(201).end()
+       } 
+    }
+    catch(err){
+        response.status(500).end()
+    }
+    
+}
