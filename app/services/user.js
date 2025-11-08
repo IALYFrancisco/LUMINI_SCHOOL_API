c4dd1b3c831d0c41405c02167876d385da799923
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-08 12:46:30 +0300

    définition du middleware qui vérifie si l'utilisateur est en même temps connecté et administrateur

--- app/services/user.js
+++ app/services/user.js
@@ -1,3 +1,11 @@
 export function GetUserInformations(request, response) {
     response.status(200).json(request.session.user)
 }
+
+export function isAdmin(request, response, next) {
+    if(request.session && request.session.user && request.session.user === "admin"){
+        next()
+    }else{
+        return response.status(401).end()
+    }
+}
