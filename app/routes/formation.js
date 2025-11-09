3a301e28616afefb848671eec8172bc57972fe34
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-09 12:45:01 +0300

    changement de middleware sur l'action d'ajout de formation

--- app/routes/formation.js
+++ app/routes/formation.js
@@ -1,9 +1,10 @@
 import Router from 'express'
 import { AddFormation, DeleteFormation, GetFormation, upload } from '../services/formation.js'
 import { isAdmin } from '../services/user.js'
+import { isAuthenticated } from '../services/authentication.js'
 
 export const formationRouter = Router()
 
-formationRouter.post('/add', isAdmin, upload.single("poster"), AddFormation)
+formationRouter.post('/add', isAuthenticated, upload.single("poster"), AddFormation)
 formationRouter.get('/get', GetFormation)
 formationRouter.delete('/delete', isAdmin, DeleteFormation)
