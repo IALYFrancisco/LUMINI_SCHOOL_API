82ba99fdf290030deef28d647c86c94873753f6e
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-13 15:13:20 +0300

    développement du service d'envoie d'email du client dans l'email de l'admin

--- app/services/email.js
+++ app/services/email.js
@@ -1,5 +1,6 @@
 export async function SendClientEmail(request, response){
     try{
+        console.log(request.body)
         response.status(200).end()
     }catch(err){
         response.status(500).end()
