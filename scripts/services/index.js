0fc0cf2a2a2affa4e0fb6488d295aa52cb4a6e2a
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-14 16:03:49 +0300

    développement du service de création de superutilisateur

--- scripts/services/index.js
+++ scripts/services/index.js
@@ -1,3 +1,22 @@
-import { DbConnection } from "../../app/services/db_connection";
-import { User } from "../../app/models/User";
-import axios from "axios";
+import chalk from 'chalk'
+import { randomBytes } from "crypto";
+import { HashPashword } from '../../app/services/authentication';
+
+
+async function createSuperuser() {
+    try{
+        console.log(chalk.yellow("Creating superuser..."))
+        let superuserPassword = randomBytes(32).toString('hex')
+        let superuserHashedPassword = await HashPashword(superuserPassword)
+        let superuser = { 
+            name: process.env.SUPERUSER_NAME,
+            email: process.env.SUPERUSER_EMAIL,
+            password: null,
+            role: 'admin'
+        }
+
+    }
+    catch(err){
+
+    }
+}
