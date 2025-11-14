5f77aaddb67ffed4bb091b24b7c36330ffa8ae16
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-14 11:11:35 +0300

    endpoint de récupération de liste des inscriptions défini

--- app/routes/registration.js
+++ app/routes/registration.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { CreateRegistration } from "../services/registration.js";
+import { CreateRegistration, GetRegistrations } from "../services/registration.js";
 import { isAuthenticated } from "../services/authentication.js";
+import { isAdmin } from "../services/user.js";
 
 export const registrationRouter = Router()
 
 registrationRouter.post('/create', isAuthenticated, CreateRegistration)
+registrationRouter.post('/get', isAdmin, GetRegistrations)
