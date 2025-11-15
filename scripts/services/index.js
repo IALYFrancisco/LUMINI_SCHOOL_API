080d7240a3b47ed3922249a18b4a6ab9d2b1ccc3
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-15 08:26:45 +0300

    amélioration toujours de service de création de superutilisateur

--- scripts/services/index.js
+++ scripts/services/index.js
@@ -5,39 +5,62 @@ import { disconnect } from "mongoose"
 import { DbConnection } from "../../app/services/db_connection.js"
 import chalk from "chalk"
 
-export async function CreateSuperuser(){
+async function CheckSuperuserAndHisEmail() {
     try{
-        console.log(chalk.bgHex('#4a78a6').hex("#fffbfc")("Création de superutilisateur."))
-        DbConnection()
+        console.log(chalk.bgHex('#4a78a6').hex("#fffbfc")("Checking superuser and his email in the database."))
+        await DbConnection()
         if(process.env.SUPERUSER_EMAIL && process.env.SUPERUSER_NAME){
             let _user = await User.findOne({email: process.env.SUPERUSER_EMAIL})
             let user = await User.findOne({status: 'superuser'})
             if(user || _user){
                 console.log(chalk.bgHex('#6e4d0cff').hex("#fffbfc")('Maybe a superuser already exist or maybe an user with provided email already exist.'))
-                return undefined
+                return true
             }else{
-                let superuserPassword = randomBytes(32).toString('hex')
-                let hashedSuperuserPassword = await HashPassword(superuserPassword)
-                let superuser = {
-                    name: process.env.SUPERUSER_NAME,
-                    email: process.env.SUPERUSER_EMAIL,
-                    status: 'superuser',
-                    password: `${hashedSuperuserPassword}`
-                }
-                let newSuperuser = new User(superuser)
-                let result = await newSuperuser.save()
-                if(result){
-                    superuser.password = superuserPassword
-                    console.log('Superuser created in the database.')
-                    return superuser
-                }
+                return false
             }
         }else{
-            console.log('Error creating superuser, SUPERUSER_NAME and SUPERUSER_EMAIL aren\'t defined.')
+            console.log(chalk.bgHex('#870202ff').hex('#fffbfc')('Error creating superuser, SUPERUSER_NAME and SUPERUSER_EMAIL aren\'t defined.'))
             return undefined
         }
     }catch(err){
-        console.log('Error creating superuser.')
+        console.log(chalk.bgHex('#870202ff').hex('#fffbfc')('Error checking superuser in the database.'))
+        return undefined
+    }finally{
+        await disconnect()
+    }
+}
+
+export async function CreateSuperuser(){
+    try{
+        console.log(chalk.bgHex('#4a78a6').hex("#fffbfc")("Création de superutilisateur."))
+        await DbConnection()
+        let checkingResult = await CheckSuperuserAndHisEmail()
+        if(checkingResult === true){
+            return undefined
+        }else if(checkingResult === undefined){
+            return undefined
+        }else{
+            let superuserPassword = randomBytes(32).toString('hex')
+            let hashedSuperuserPassword = await HashPassword(superuserPassword)
+            let superuser = {
+                name: process.env.SUPERUSER_NAME,
+                email: process.env.SUPERUSER_EMAIL,
+                status: 'superuser',
+                password: `${hashedSuperuserPassword}`
+            }
+            let newSuperuser = new User(superuser)
+            let result = await newSuperuser.save()
+            if(result){
+                superuser.password = superuserPassword
+                console.log(chalk.bgHex('#098702ff').hex('#fffbfc')('Superuser created in the database.'))
+                return superuser
+            }else{
+                console.log(chalk.bgHex('#870202ff').hex('#fffbfc')('Error saving superuser in the database.'))
+                return undefined
+            }
+        }
+    }catch(err){
+        console.log(chalk.bgHex('#870202ff').hex('#fffbfc')('Error creating superuser.'))
         return undefined
     }finally{
         await disconnect()
