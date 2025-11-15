b7f4b8ab90d65d53cd3e2af4e310effe9d16d4a9
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-15 09:31:49 +0300

    service de création de superutilisateur et ajout dans la base de donnée développé

--- scripts/services/index.js
+++ scripts/services/index.js
@@ -1,14 +1,11 @@
 import { randomBytes } from "crypto"
 import { HashPassword } from "../../app/services/authentication.js"
 import { User } from "../../app/models/User.js"
-import { disconnect } from "mongoose"
-import { DbConnection } from "../../app/services/db_connection.js"
 import chalk from "chalk"
 
 async function CheckSuperuserAndHisEmail() {
     try{
         console.log(chalk.bgHex('#4a78a6').hex("#fffbfc")("Checking superuser and his email in the database."))
-        await DbConnection()
         if(process.env.SUPERUSER_EMAIL && process.env.SUPERUSER_NAME){
             let _user = await User.findOne({email: process.env.SUPERUSER_EMAIL})
             let user = await User.findOne({status: 'superuser'})
@@ -25,22 +22,20 @@ async function CheckSuperuserAndHisEmail() {
     }catch(err){
         console.log(chalk.bgHex('#870202ff').hex('#fffbfc')('Error checking superuser in the database.'))
         return undefined
-    }finally{
-        await disconnect()
     }
 }
+
+export const superuserPassword = randomBytes(32).toString('hex')
 
 export async function CreateSuperuser(){
     try{
         console.log(chalk.bgHex('#4a78a6').hex("#fffbfc")("Création de superutilisateur."))
-        await DbConnection()
         let checkingResult = await CheckSuperuserAndHisEmail()
         if(checkingResult === true){
             return undefined
         }else if(checkingResult === undefined){
             return undefined
         }else{
-            let superuserPassword = randomBytes(32).toString('hex')
             let hashedSuperuserPassword = await HashPassword(superuserPassword)
             let superuser = {
                 name: process.env.SUPERUSER_NAME,
@@ -51,18 +46,16 @@ export async function CreateSuperuser(){
             let newSuperuser = new User(superuser)
             let result = await newSuperuser.save()
             if(result){
-                superuser.password = superuserPassword
                 console.log(chalk.bgHex('#098702ff').hex('#fffbfc')('Superuser created in the database.'))
-                return superuser
+                return true
             }else{
                 console.log(chalk.bgHex('#870202ff').hex('#fffbfc')('Error saving superuser in the database.'))
                 return undefined
             }
         }
     }catch(err){
+        console.log(err)
         console.log(chalk.bgHex('#870202ff').hex('#fffbfc')('Error creating superuser.'))
         return undefined
-    }finally{
-        await disconnect()
     }
 }
