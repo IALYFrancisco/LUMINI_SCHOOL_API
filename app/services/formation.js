c5be2812015b88f8c07949becd3064c93d0ddcb7
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-20 12:43:52 +0300

    service de mis à jour de formation défini

--- app/services/formation.js
+++ app/services/formation.js
@@ -48,8 +48,10 @@ export async function GetFormation(request, response){
 
 export async function UpdateFormation(request, response) {
     try{
-        
-        response.status(200).end()
+        let formation = await Formation.findByIdAndUpdate(request.body.formationId, request.body.update)
+        if(formation){
+            response.status(200).end()
+        }
     }
     catch(err){
         response.status(500).end()
