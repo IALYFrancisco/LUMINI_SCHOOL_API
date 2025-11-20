f77676dce6eaaf13518336bb47f739e2eb660996
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-20 08:04:02 +0300

    ajout de middleware isAdminOrSuperuser sur l'endpoint de récupération de liste d'inscriptions

--- app/routes/registration.js
+++ app/routes/registration.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { CreateRegistration, GetRegistrations } from "../services/registration.js";
 import { isAuthenticated } from "../services/authentication.js";
+import { isAdminOrSuperuser } from "../services/user.js";
 
 export const registrationRouter = Router()
 
 registrationRouter.post('/create', isAuthenticated, CreateRegistration)
-registrationRouter.get('/get', isAuthenticated, GetRegistrations)
+registrationRouter.get('/get', isAdminOrSuperuser, GetRegistrations)
