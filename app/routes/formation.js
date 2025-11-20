f6a58ca30200bcb706ec5155272294e51edd76d8
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-20 08:02:28 +0300

    ajout de middleware de protéction aux routes de formation

--- app/routes/formation.js
+++ app/routes/formation.js
@@ -1,10 +1,9 @@
 import Router from 'express'
 import { AddFormation, DeleteFormation, GetFormation, upload } from '../services/formation.js'
-import { isAdmin } from '../services/user.js'
-import { isAuthenticated } from '../services/authentication.js'
+import { isAdminOrSuperuser } from '../services/user.js'
 
 export const formationRouter = Router()
 
-formationRouter.post('/add', isAuthenticated, upload.single("poster"), AddFormation)
+formationRouter.post('/add', isAdminOrSuperuser, upload.single("poster"), AddFormation)
 formationRouter.get('/get', GetFormation)
-formationRouter.delete('/delete', isAdmin, DeleteFormation)
+formationRouter.delete('/delete', isAdminOrSuperuser, DeleteFormation)
