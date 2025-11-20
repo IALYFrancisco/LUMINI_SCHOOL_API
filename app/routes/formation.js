83d450ce54b1661f3917aecd92a68dd2a89a3a4f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-20 08:43:35 +0300

    définition de l'endpoint de mis à jour de formation

--- app/routes/formation.js
+++ app/routes/formation.js
@@ -1,9 +1,10 @@
 import Router from 'express'
-import { AddFormation, DeleteFormation, GetFormation, upload } from '../services/formation.js'
+import { AddFormation, DeleteFormation, GetFormation, UpdateFormation, upload } from '../services/formation.js'
 import { isAdminOrSuperuser } from '../services/user.js'
 
 export const formationRouter = Router()
 
 formationRouter.post('/add', isAdminOrSuperuser, upload.single("poster"), AddFormation)
 formationRouter.get('/get', GetFormation)
+formationRouter.patch('/update', isAdminOrSuperuser, UpdateFormation)
 formationRouter.delete('/delete', isAdminOrSuperuser, DeleteFormation)
