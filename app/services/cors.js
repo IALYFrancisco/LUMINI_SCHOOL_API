2420e1f3076d2b812f4a2e5654785c8cf566a5bb
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-20 12:35:34 +0300

    ajout de méthode CORS dans l'autorisation CORS

--- app/services/cors.js
+++ app/services/cors.js
@@ -1,7 +1,7 @@
 export const corsConfiguration = (request, response, next) => {
     response.setHeader('Access-Control-Allow-Origin', process.env.ALLOWED_ORIGIN)
     response.setHeader('Access-Control-Allow-Headers', process.env.ALLOWED_HEADERS)
-    response.setHeader('Access-Control-Allow-Methods', 'DELETE')
+    response.setHeader('Access-Control-Allow-Methods', 'PATCH,DELETE')
     response.setHeader('Access-Control-Allow-Credentials', true)
     next()
 }
