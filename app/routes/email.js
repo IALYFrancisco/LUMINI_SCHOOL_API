a228181a4db597f84ea167ab31bd1735324f8d28
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-20 07:57:27 +0300

    ajout de middleware isAuthenticated sur l'endpoint /send

--- app/routes/email.js
+++ app/routes/email.js
@@ -1,6 +1,7 @@
 import Router from 'express'
 import { SendClientEmail } from '../services/email.js'
+import { isAuthenticated } from '../services/authentication.js'
 
 export const emailRouter = Router()
 
-emailRouter.post('/send', SendClientEmail)
+emailRouter.post('/send', isAuthenticated, SendClientEmail)
