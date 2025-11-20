346b5e4281f258815864d7d05d745b1982138922
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-20 08:00:42 +0300

    changement du middleware isAdmin en isAdminOrSuperuser

--- app/services/user.js
+++ app/services/user.js
@@ -2,8 +2,8 @@ export function GetUserInformations(request, response) {
     response.status(200).json(request.session.user)
 }
 
-export function isAdmin(request, response, next) {
-    if(request.session && request.session.user && request.session.user.status === "superuser"){
+export function isAdminOrSuperuser(request, response, next) {
+    if(request.session && request.session.user && (request.session.user.status === "superuser" || request.session.user.status === "admin")){
         next()
     }else{
         return response.status(401).end()
