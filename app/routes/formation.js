ee52062d2107e1edc7c5f674a3c674682939ea00
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-11-21 08:27:31 +0300

    endpoint de mis à jour de formation défini

--- app/routes/formation.js
+++ app/routes/formation.js
@@ -1,5 +1,5 @@
 import Router from 'express'
-import { AddFormation, DeleteFormation, GetFormation, FormationPublication, upload } from '../services/formation.js'
+import { AddFormation, DeleteFormation, GetFormation, FormationPublication, upload, UpdateFormation } from '../services/formation.js'
 import { isAdminOrSuperuser } from '../services/user.js'
 
 export const formationRouter = Router()
@@ -7,5 +7,5 @@ export const formationRouter = Router()
 formationRouter.post('/add', isAdminOrSuperuser, upload.single("poster"), AddFormation)
 formationRouter.get('/get', GetFormation)
 formationRouter.patch('/publication', isAdminOrSuperuser, FormationPublication)
-formationRouter.put('/update', isAdminOrSuperuser, FormationPublication)
+formationRouter.put('/update', isAdminOrSuperuser, UpdateFormation)
 formationRouter.delete('/delete', isAdminOrSuperuser, DeleteFormation)
