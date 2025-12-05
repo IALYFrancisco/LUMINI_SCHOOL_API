d0d722acd55a3107f169b8ee5bfc87b09e95dd9f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-05 09:32:06 +0300

    endpoint d'ajout de fichier développé

--- app/routes/article.js
+++ app/routes/article.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { AddArticle, AddIllustration, GetArticle } from "../services/article.js";
+import { AddArticle, AddFile, AddIllustration, GetArticle } from "../services/article.js";
 import { isAdminOrSuperuser } from "../services/user.js";
 import { upload } from "../services/formation.js";
 
@@ -8,3 +8,4 @@ export const articleRouter = Router()
 articleRouter.get('/get', GetArticle)
 articleRouter.post('/create', isAdminOrSuperuser, AddArticle)
 articleRouter.post('/add-illustration', isAdminOrSuperuser, upload.single("image"), AddIllustration)
+articleRouter.post('/add-file', isAdminOrSuperuser, upload.single("file", AddFile))
