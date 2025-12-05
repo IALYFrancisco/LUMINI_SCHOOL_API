3167fd1d9feb189e6b18e38a01d2023ee20b5689
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-05 06:18:08 +0300

    endpoint de récupération de liste d'article défini

--- app/routes/article.js
+++ app/routes/article.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { AddArticle } from "../services/article.js";
+import { AddArticle, GetArticle } from "../services/article.js";
 
 export const articleRouter = Router()
 
+articleRouter.get('/get', GetArticle)
 articleRouter.post('/create', AddArticle)
