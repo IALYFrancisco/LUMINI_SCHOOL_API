ba4fd3fc22979ca5d7b34724aa49092f58beb828
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-06 08:53:03 +0300

    endpoint de publication et dépublication d'article défini

--- app/routes/article.js
+++ app/routes/article.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { AddArticle, AddFile, AddIllustration, GetArticle, uploadFile } from "../services/article.js";
+import { AddArticle, AddFile, AddIllustration, ArticlePublication, GetArticle, uploadFile } from "../services/article.js";
 import { isAdminOrSuperuser } from "../services/user.js";
 import { upload } from "../services/formation.js";
 
@@ -9,3 +9,4 @@ articleRouter.get('/get', GetArticle)
 articleRouter.post('/create', isAdminOrSuperuser, upload.single("image"), AddArticle)
 articleRouter.post('/add-illustration', isAdminOrSuperuser, upload.single("image"), AddIllustration)
 articleRouter.post('/add-file', isAdminOrSuperuser, uploadFile.single("file"), AddFile)
+articleRouter.patch('/publication', isAdminOrSuperuser, ArticlePublication)
