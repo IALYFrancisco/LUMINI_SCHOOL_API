70c0a3f4f4697df4234b189e0ae1216f73d2f834
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-08 05:36:33 +0300

    endpoint de récupération de liste des utilisateurs défini

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,7 +1,8 @@
 import Router from 'express'
 import { isAuthenticated } from '../services/authentication.js'
-import { GetUserInformations } from '../services/user.js'
+import { GetUserInformations, isSuperuser } from '../services/user.js'
 
 export const userRouter = Router()
 
 userRouter.get('/informations', isAuthenticated, GetUserInformations)
+userRouter.get('/get', isSuperuser, GetUserInformations)
