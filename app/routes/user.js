359d14a6bad4dcb0376402f0d5dec3c334019ca4
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-09 09:10:41 +0300

    endpoint de suppression de compte défini

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,9 +1,10 @@
 import Router from 'express'
 import { isAuthenticated } from '../services/authentication.js'
-import { ChangeUserStatus, GetUser, GetUserInformations, isSuperuser } from '../services/user.js'
+import { ChangeUserStatus, DeleteAccount, GetUser, GetUserInformations, isSuperuser } from '../services/user.js'
 
 export const userRouter = Router()
 
 userRouter.get('/informations', isAuthenticated, GetUserInformations)
 userRouter.get('/get', isSuperuser, GetUser)
 userRouter.patch('/change-status', isSuperuser, ChangeUserStatus)
+userRouter.delete('/delete', isAuthenticated, DeleteAccount)
