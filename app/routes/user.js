caca381d69f71b5de7c535e7783f4bcbabb3199b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-09 14:14:41 +0300

    endpoint de changement de mot de passe défini

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,10 +1,11 @@
 import Router from 'express'
 import { isAuthenticated } from '../services/authentication.js'
-import { ChangeUserStatus, DeleteAccount, GetUser, GetUserInformations, isSuperuser } from '../services/user.js'
+import { ChangePassword, ChangeUserStatus, DeleteAccount, GetUser, GetUserInformations, isSuperuser } from '../services/user.js'
 
 export const userRouter = Router()
 
 userRouter.get('/informations', isAuthenticated, GetUserInformations)
 userRouter.get('/get', isSuperuser, GetUser)
 userRouter.patch('/change-status', isSuperuser, ChangeUserStatus)
+userRouter.patch('/change-password', isAuthenticated, ChangePassword)
 userRouter.delete('/delete', isAuthenticated, DeleteAccount)
