17a39b27c40d421632392b7f2968d46332332d23
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-10 08:02:32 +0300

    endpoint de mis à jour utilisateur défini

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,6 +1,7 @@
 import Router from 'express'
 import { isAuthenticated } from '../services/authentication.js'
-import { ChangePassword, ChangeUserStatus, DeleteAccount, GetUser, GetUserInformations, isSuperuser } from '../services/user.js'
+import { ChangePassword, ChangeUserStatus, DeleteAccount, GetUser, GetUserInformations, isSuperuser, UpdateUser } from '../services/user.js'
+import { upload } from '../services/formation.js'
 
 export const userRouter = Router()
 
@@ -8,4 +9,5 @@ userRouter.get('/informations', isAuthenticated, GetUserInformations)
 userRouter.get('/get', isSuperuser, GetUser)
 userRouter.patch('/change-status', isSuperuser, ChangeUserStatus)
 userRouter.patch('/change-password', isAuthenticated, ChangePassword)
+userRouter.patch('/update', isAuthenticated, upload.single('profile'),UpdateUser)
 userRouter.delete('/delete', isAuthenticated, DeleteAccount)
