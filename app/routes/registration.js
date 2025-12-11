8bad398f56f429427070123efae289345f8ae6b2
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-11 13:45:24 +0300

    changement du protéction sur le service GetRegistrations

--- app/routes/registration.js
+++ app/routes/registration.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import { CreateRegistration, GetRegistrations } from "../services/registration.js";
 import { isAuthenticated } from "../services/authentication.js";
-import { isAdminOrSuperuser } from "../services/user.js";
 
 export const registrationRouter = Router()
 
 registrationRouter.post('/create', isAuthenticated, CreateRegistration)
-registrationRouter.get('/get', isAdminOrSuperuser, GetRegistrations)
+registrationRouter.get('/get', isAuthenticated, GetRegistrations)
