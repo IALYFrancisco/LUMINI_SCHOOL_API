a8c325295f9541787c17ce676d3e9380d60d4d29
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-11 15:35:56 +0300

    mis à jour du modèle de donnée Registration

--- app/models/Registration.js
+++ app/models/Registration.js
@@ -3,7 +3,8 @@ import { model, Schema } from "mongoose"
 const registrationSchema = new Schema({
     formation_id: { type: Schema.Types.ObjectId, ref: "Formation", required: true },
     user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    registrationDate: { type: Date, required: true, default: Date.now() }
+    registrationDate: { type: Date, required: true, default: Date.now() },
+    coursePricePayed: { type: Boolean, required: true, default: false }
 })
 
 export const Registration = new model('Registrations', registrationSchema)
