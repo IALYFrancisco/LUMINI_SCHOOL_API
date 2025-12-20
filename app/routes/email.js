deec3eff3696574043127b5d3d6407e87c73c889
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-12-20 18:03:02 +0300

    suppression d'un import inutile

--- app/routes/email.js
+++ app/routes/email.js
@@ -1,6 +1,5 @@
 import Router from 'express'
 import { SendClientEmail } from '../services/email.js'
-import { isAuthenticated } from '../services/authentication.js'
 
 export const emailRouter = Router()
 
