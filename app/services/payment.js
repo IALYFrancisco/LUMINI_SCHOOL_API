a95ada10b964522ace5e380b79f97bf46503b4d7
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-01-05 21:06:30 +0300

    développement encore des services de génération de token pour les paiements mvola

--- app/services/payment.js
+++ app/services/payment.js
@@ -1,7 +1,14 @@
-export const GenerateAccessToken = () => {
-    console.log("Hello")
+import axios from "axios"
+
+const credentials = Buffer.from(`${process.env.MVOLA_CONSUMER_KEY}:${process.env.MVOLA_CONSUMER_SECRET}`).toString("base64")
+
+export const GenerateAccessToken = ( request, response, next ) => {
+    axios.post(`${process.env.MVOLA_API_ENDPOINT}/token`, {}, { headers: { Authorization: `Basic ${credentials}` } })
+    .then()
+    .catch()
+    .finally(()=>{ next() })
 }
 
-export function AccessTokenGenerationEveryHour(){
+export function RefreshToken(){
     setInterval( GenerateAccessToken , 55 * 60 * 1_000)
 }
